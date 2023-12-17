1e93343e0f0ac2ecd5e36424de28fb45562192c5
Vinicius Peres <viniciusgperes@hotmail.com>
2023-12-17 19:27:25 -0300

    main file, cors vercel back

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: ['http://localhost:3000'],
+    origin: ['http://localhost:3000', 'https://aimob-backend.vercel.app'],
   });
 
   app.setGlobalPrefix('/api/v1/');
