f0667dba97c9a92c74b0cc4498f3c7a1990824bf
Vinicius Peres <viniciusgperes@hotmail.com>
2024-08-14 21:21:21 -0300

    feat: update new real estates

--- src/realestates/realestates.controller.ts
+++ src/realestates/realestates.controller.ts
@@ -3,35 +3,46 @@ import {
   Get,
   Post,
   Body,
-  Put,
+  Patch,
   Param,
   Delete,
+  Req,
+  Query,
+  UseInterceptors,
 } from '@nestjs/common';
 import { RealestatesService } from './realestates.service';
 import { RealEstate } from '@prisma/client';
+import { AuthRequest } from 'src/auth/models/AuthRequest';
+import { FilesInterceptor } from '@nestjs/platform-express';
 import { IsPublic } from 'src/auth/decorators/is-public.decorator';
 
 @Controller('realestates')
 export class RealestatesController {
   constructor(private readonly realestatesService: RealestatesService) {}
 
-  @IsPublic()
   @Post()
-  create(@Body() realEstate: RealEstate) {
-    return this.realestatesService.create(realEstate);
+  @UseInterceptors(FilesInterceptor('images'))
+  create(@Body() realEstate: RealEstate, @Req() request: AuthRequest) {
+    return this.realestatesService.create(realEstate, request);
   }
-
+  @IsPublic()
   @Get()
   findAll(): Promise<RealEstate[]> {
     return this.realestatesService.findAll();
   }
-
+  @IsPublic()
   @Get(':id')
   findOne(@Param('id') id: number): Promise<RealEstate | null> {
     return this.realestatesService.findOne(+id);
   }
 
-  @Put(':id')
+  @IsPublic()
+  @Get('search')
+  searchProjects(@Query('type') type: string): Promise<RealEstate[] | null> {
+    return this.realestatesService.searchRealEstate(type);
+  }
+
+  @Patch(':id')
   update(
     @Param('id') id: number,
     @Body() realEstate: RealEstate,
--- src/realestates/realestates.service.ts
+++ src/realestates/realestates.service.ts
@@ -1,25 +1,14 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { PrismaClient, RealEstate } from '@prisma/client';
+import { AuthRequest } from 'src/auth/models/AuthRequest';
 
 @Injectable()
 export class RealestatesService {
   constructor(private prisma: PrismaClient) {}
 
-  async create(data: any): Promise<RealEstate> {
-    const { userId, clientUserId, ...realEstateData } = data;
-
-    const creator = await this.prisma.user.findUnique({
-      where: { id: userId },
-    });
-
-    const client = await this.prisma.user.findUnique({
-      where: { id: clientUserId },
-    });
-
-    if (!creator || !client) {
-      throw new NotFoundException('User not found.');
-    }
-
+  async create(data: any, request: AuthRequest) {
+    const { ...realEstateData } = data;
+    const userId = request.user.id;
     const existingRealEstate = await this.prisma.realEstate.findUnique({
       where: { registration: realEstateData.registration },
     });
@@ -29,12 +18,12 @@ export class RealestatesService {
         'RealEstate with this registration already exists.',
       );
     }
+    console.log('User ID:', userId);
 
     const createdRealEstate = await this.prisma.realEstate.create({
       data: {
         ...realEstateData,
-        employee: { connect: { id: userId } },
-        client: { connect: { id: clientUserId } },
+        userId: userId,
       },
     });
 
@@ -42,26 +31,29 @@ export class RealestatesService {
   }
 
   async findAll(): Promise<RealEstate[]> {
-    const foundAllRealEstate = await this.prisma.realEstate.findMany({
-      include: {
-        employee: true,
-        client: true,
-      },
-    });
+    const foundAllRealEstate = await this.prisma.realEstate.findMany();
+
     return foundAllRealEstate;
   }
 
   async findOne(id: number): Promise<RealEstate> {
     const foundOneRealEstate = await this.prisma.realEstate.findUnique({
       where: { id },
-      include: {
-        employee: true,
-        client: true,
-      },
     });
     return foundOneRealEstate;
   }
 
+  async searchRealEstate(type: string): Promise<RealEstate[]> {
+    const searchProjects = await this.prisma.realEstate.findMany({
+      where: {
+        type: {
+          contains: type,
+          mode: 'insensitive',
+        },
+      },
+    });
+    return searchProjects;
+  }
   async update(id: number, data: Partial<RealEstate>): Promise<RealEstate> {
     const updatedRealEstate = await this.prisma.realEstate.update({
       where: { id },
