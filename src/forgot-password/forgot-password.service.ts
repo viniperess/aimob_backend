3c1d6940336ade52c146c74bb78cba3d2403fd5c
Vinicius Peres <viniciusgperes@hotmail.com>
2024-11-01 21:41:03 -0300

    fix: updating passwords for the email

--- src/forgot-password/forgot-password.service.ts
+++ src/forgot-password/forgot-password.service.ts
@@ -27,7 +27,7 @@ export class ForgotPasswordService {
     });
 
     const transporter = nodemailer.createTransport({
-      service: 'hotmail',
+      service: 'gmail',
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
